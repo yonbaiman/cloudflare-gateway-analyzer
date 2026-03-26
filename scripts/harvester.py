55832badb8c397180cc29a2e00f777e5fe09c21b
yonbaiman <252968841+yonbaiman@users.noreply.github.com>
2026-03-26 09:49:15 +0900

    Update harvester.py

--- scripts/harvester.py
+++ scripts/harvester.py
@@ -26,7 +26,7 @@ def main():
           viewer {
             accounts(filter: {accountTag: "%s"}) {
               gatewayResolverQueriesAdaptiveGroups(
-                limit: 20000, 
+                limit: 10000, 
                 filter: {datetime_geq: "%s", datetime_leq: "%s"},
                 orderBy: [datetime_DESC]
               ) {
